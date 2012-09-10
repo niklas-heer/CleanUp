633056681760f2d6234dfc3dd615754337c58650
Niklas Heer <niklas.heer@me.com>
2012-09-10 20:37:16 +0200

    Cleaned and added Icon

--- cleanUp.py
+++ cleanUp.py
@@ -2,50 +2,75 @@ from send2trash import send2trash
 import os
 import gntp.notifier
 import sys
+import urllib
 
-folder = "/Users/nh/REPOS/git/sellaround"
-exception = "empty"
-num = 0
-message =""
-for f in sys.argv[1:]:
-    folder = f
-
-for the_file in os.listdir(folder):
-    file_path = os.path.join(folder, the_file)
+def download(url):
+    """Copy the contents of a file from a given URL
+    to a local file.
+    """
+    home = os.getenv("HOME")
+    folder = home + "/Library/Application Support/NH_CleanUp/"
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    if not os.path.exists(folder + url.split('/')[-1]):
+        webFile = urllib.urlopen(url)
+        localFile = open(folder + url.split('/')[-1], 'w')
+        localFile.write(webFile.read())
+        webFile.close()
+        localFile.close()
+    return (folder + url.split('/')[-1])
+    
+if __name__ == '__main__':
+    folder = ""
+    exception = "empty"
+    num = 0
+    message =""
+    image = ""
+    
+    ### image runterladen
     try:
-        if not (os.path.isfile(file_path)):
-            if (os.path.basename(file_path) == "mariaTmp"):
-                subfolder1 = folder + "/" + os.path.basename(file_path)
-                for the_file in os.listdir(subfolder1):
-                    file_path = os.path.join(subfolder1, the_file)
-                    if not (os.path.basename(file_path) == exception):
-                        send2trash(file_path)
-                        num += 1
-            if (os.path.basename(file_path) == "www"):
-                subfolder2 = folder + "/" + os.path.basename(file_path) + "/" + "mariaTmp"
-                for the_file in os.listdir(subfolder2):
-                    file_path = os.path.join(subfolder2, the_file)
-                    if not (os.path.basename(file_path) == exception):
-                        send2trash(file_path)
-                        num += 1
-    except Exception, e:
-        print e
+        image = open(download("http://niklas-heer.de/pics/trash.png")).read()
+    except IOError:
+        print 'Filename not found.'
+    
+    for f in sys.argv[1:]:
+        folder = f
+
+    for the_file in os.listdir(folder):
+        file_path = os.path.join(folder, the_file)
+        try:
+            if not (os.path.isfile(file_path)):
+                if (os.path.basename(file_path) == "mariaTmp"):
+                    subfolder1 = folder + "/" + os.path.basename(file_path)
+                    for the_file in os.listdir(subfolder1):
+                        file_path = os.path.join(subfolder1, the_file)
+                        if not (os.path.basename(file_path) == exception):
+                            send2trash(file_path)
+                            num += 1
+                if (os.path.basename(file_path) == "www"):
+                    subfolder2 = folder + "/" + os.path.basename(file_path) + "/" + "mariaTmp"
+                    for the_file in os.listdir(subfolder2):
+                        file_path = os.path.join(subfolder2, the_file)
+                        if not (os.path.basename(file_path) == exception):
+                            send2trash(file_path)
+                            num += 1
+        except Exception, e:
+            print e
 
 
-message = str(num) + ' files deleted!'
-image = open('/Users/nh/Pictures/trash.png').read()
-growl = gntp.notifier.GrowlNotifier(
-    applicationName = "My Application Name",
-    notifications = ["New Updates","New Messages"],
-    defaultNotifications = ["New Messages"],
-)
-growl.register()
+    message = str(num) + ' files deleted!'
+    growl = gntp.notifier.GrowlNotifier(
+        applicationName = "My Application Name",
+        notifications = ["New Updates","New Messages"],
+        defaultNotifications = ["New Messages"],
+    )
+    growl.register()
 
-growl.notify(
-    noteType = "New Messages",
-    title = "mariaTmp cleaned!",
-    description = message,
-	icon = image,
-    sticky = False,
-    priority = 1,
-)
+    growl.notify(
+        noteType = "New Messages",
+        title = "mariaTmp cleaned!",
+        description = message,
+    	icon = image,
+        sticky = False,
+        priority = 1,
+    )
